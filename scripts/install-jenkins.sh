fbb4e20bf630db9b9ea0b8d0933b5c041dae01fe
Tanseer <tanseer@skillminds.com>
2025-06-19 15:36:08 +0530

    added shell script for installing jenkins

---
+++ scripts/install-jenkins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install JDk 
+sudo apt install fontconfig openjdk-17-jre -y
+
+# Add the Jenkins repository key to the system
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# Add the Jenkins repository to the sources list
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update the package list
+sudo apt-get update -y
+
+# Install Jenkins
+sudo apt-get install jenkins -y
+
+# Check if Jenkins is installed successfully
+jenkins --version
