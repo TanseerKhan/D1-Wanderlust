dd3d98a752f3e9844d9a030105d549eef8c82cba
Tanseer <tanseer@skillminds.com>
2025-06-20 14:22:42 +0530

    added script to install aws cli on ubuntu

---
+++ scripts/install-aws-cli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Downlaod aws cli v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Install unzip if not already installed
+sudo apt install unzip
+
+# Unzip the downloaded file
+unzip awscliv2.zip
+
+# Run the installer
+sudo ./aws/install
