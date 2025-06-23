87ed8053d790b31d20bca81ad9d105f5bcb57563
Tanseer <tanseer@skillminds.com>
2025-06-23 16:05:08 +0530

    minor changes

--- scripts/install-kubectl.sh
+++ scripts/install-kubectl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e # Exit on error
 
-#Download the latest stable version of kubectl
+# Download the latest stable version of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-#Make it executable
+# Make it executable
 chmod +x kubectl
 
-#Move to /usr/local/bin
+# Move to /usr/local/bin
 sudo mv kubectl /usr/local/bin/
 
-#Verify installation
+# Verify installation
 kubectl version --client
